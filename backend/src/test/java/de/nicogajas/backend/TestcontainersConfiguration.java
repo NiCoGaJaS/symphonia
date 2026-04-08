984997142626c7d807827102c3e0f3adf58ae9d7
Connor Schweighöfer <squidxtv@gmail.com>
2026-04-08 14:18:57 +0200

    diffplug/spotless formatting and cicd integration (#1)

--- backend/src/test/java/de/nicogajas/backend/TestcontainersConfiguration.java
+++ backend/src/test/java/de/nicogajas/backend/TestcontainersConfiguration.java
@@ -8,11 +8,11 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-	}
-
+    
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
+    
 }
