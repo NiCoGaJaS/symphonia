630c94c32042030136353c4d51d2e802ef4b0179
gabrielakbarov <kontakt@gabrielakbarov.de>
2026-04-09 15:52:03 +0200

    fix tests

--- frontend/src/app/components/global/menu/menu.component.spec.ts
+++ frontend/src/app/components/global/menu/menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../app.routes';
 
 describe('MenuComponent', () => {
     let component: MenuComponent;
@@ -9,6 +11,7 @@ describe('MenuComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [MenuComponent],
+            providers: [provideRouter(routes)],
         }).compileComponents();
 
         fixture = TestBed.createComponent(MenuComponent);
