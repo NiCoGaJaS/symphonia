351b80c2d99f811d4388d0ededa80899ec79dafc
Yousername01 <jannik_michael.abram@edu.fhdw.de>
2026-04-20 17:42:32 +0200

    refactor(footer): use top-level styles

--- frontend/src/app/components/global/footer/footer.component.ts
+++ frontend/src/app/components/global/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
     selector: 'app-footer',
-    imports: [ToggleSwitchModule, FormsModule],
+    imports: [ToggleSwitchModule],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
 })
