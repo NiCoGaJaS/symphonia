98fe145b0a6caa78a1f1a3c584e0c402eb3501a9
gabrielakbarov <kontakt@gabrielakbarov.de>
2026-04-24 13:57:24 +0200

    Fix port for CORS config

--- backend/src/main/java/de/nicogajas/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/nicogajas/backend/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:4000")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
