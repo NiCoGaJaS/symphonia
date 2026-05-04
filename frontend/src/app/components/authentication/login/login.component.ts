729be50e0889ccc850291b29110fbca1c3bd1b7a
gabrielakbarov <kontakt@gabrielakbarov.de>
2026-05-04 16:54:07 +0200

    Change inputmask to inputtext

--- frontend/src/app/components/authentication/login/login.component.ts
+++ frontend/src/app/components/authentication/login/login.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
-import {Avatar} from 'primeng/avatar';
-import {InputGroup} from 'primeng/inputgroup';
-import {InputGroupAddon} from 'primeng/inputgroupaddon';
-import {FloatLabel} from 'primeng/floatlabel';
-import {InputMask} from 'primeng/inputmask';
+import { Component } from '@angular/core';
+import { Avatar } from 'primeng/avatar';
+import { InputGroup } from 'primeng/inputgroup';
+import { InputGroupAddon } from 'primeng/inputgroupaddon';
+import { InputText } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -13,10 +14,11 @@ import {InputMask} from 'primeng/inputmask';
         InputGroup,
         InputGroupAddon,
         FloatLabel,
-        InputMask
+        FormsModule,
+        InputText,
     ],
-    styleUrl: 'login.component.css'
+    styleUrl: 'login.component.css',
 })
 export class Login {
-
+    value = '';
 }
