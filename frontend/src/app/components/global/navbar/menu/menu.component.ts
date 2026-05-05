b9578b8ae172e5e18acfaac83e6918759f03d6c3
Connor Schweighöfer <squidxtv@gmail.com>
2026-05-05 16:04:46 +0200

    Fix navbar and center elements properly

--- frontend/src/app/components/global/navbar/menu/menu.component.ts
+++ frontend/src/app/components/global/navbar/menu/menu.component.ts
@@ -17,66 +17,36 @@ export class MenuComponent implements OnInit {
             {
                 label: 'Gitarren',
                 items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Item', routerLink: '/' },
-                                { label: 'Item', routerLink: '/' },
-                            ],
-                        },
-                    ],
+                    [{ label: 'Item', routerLink: '/' }],
+                    [{ label: 'Item', routerLink: '/' }],
                 ],
             },
             {
                 label: 'Klaviere',
                 items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Item', routerLink: '/' },
-                                { label: 'Item', routerLink: '/' },
-                            ],
-                        },
-                    ],
+                    [{ label: 'Item', routerLink: '/' }],
+                    [{ label: 'Item', routerLink: '/' }],
                 ],
             },
             {
                 label: 'Drums',
                 items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Item', routerLink: '/' },
-                                { label: 'Item', routerLink: '/' },
-                            ],
-                        },
-                    ],
+                    [{ label: 'Item', routerLink: '/' }],
+                    [{ label: 'Item', routerLink: '/' }],
                 ],
             },
             {
                 label: 'Schallplatten',
                 items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Item', routerLink: '/' },
-                                { label: 'Item', routerLink: '/' },
-                            ],
-                        },
-                    ],
+                    [{ label: 'Item', routerLink: '/' }],
+                    [{ label: 'Item', routerLink: '/' }],
                 ],
             },
             {
                 label: 'Zubehör',
                 items: [
-                    [
-                        {
-                            items: [
-                                { label: 'Item', routerLink: '/' },
-                                { label: 'Item2', routerLink: '/' },
-                            ],
-                        },
-                    ],
+                    [{ label: 'Item', routerLink: '/' }],
+                    [{ label: 'Item2', routerLink: '/' }],
                 ],
             },
         ];
--- frontend/src/app/components/global/navbar/navbar.component.spec.ts
+++ frontend/src/app/components/global/navbar/navbar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 import { provideRouter } from '@angular/router';
-import { routes } from '../../../app.routes';
+import { routes } from '@app/app.routes';
 
 describe('NavbarComponent', () => {
     let component: NavbarComponent;
