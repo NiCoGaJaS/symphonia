66fb4bb4ab04b70338a333a510ab867128f0d0f5
Connor Schweighöfer <squidxtv@gmail.com>
2026-05-12 18:54:46 +0200

    Fix linting

--- frontend/src/app/components/global/navbar/menu/menu.component.ts
+++ frontend/src/app/components/global/navbar/menu/menu.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import {Menubar} from "primeng/menubar";
+import { Menubar } from 'primeng/menubar';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
     imports: [Menubar],
     templateUrl: './menu.component.html',
-    styleUrl: './menu.component.css'
+    styleUrl: './menu.component.css',
 })
 export class MenuComponent implements OnInit {
     items: MenuItem[] | undefined;
