b05591e8d3138921f3f8dc682789d9ce29fe6d97
Jannik <jannik_michael.abram@edu.fhdw.de>
2026-05-18 13:19:17 +0200

    Make footer responsive (#67)
    Co-authored-by: Connor Schweighöfer <squidxtv@gmail.com>

--- frontend/src/app/components/global/footer/footer.component.spec.ts
+++ frontend/src/app/components/global/footer/footer.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { provideRouter } from '@angular/router';
-import { routes } from '../../../app.routes';
+import { routes } from '@app/app.routes';
 
 describe('FooterComponent', () => {
     let component: FooterComponent;
--- frontend/src/app/components/global/footer/footer.component.ts
+++ frontend/src/app/components/global/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Divider } from 'primeng/divider';
 import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
     selector: 'app-footer',
-    imports: [ToggleSwitchModule],
+    imports: [ToggleSwitchModule, Divider],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
 })
