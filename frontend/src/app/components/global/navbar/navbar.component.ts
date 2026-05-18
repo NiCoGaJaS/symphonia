d0c6aaeb7bb0f28d8cc8e9b920f1df98394ed23d
gabrielakbarov <kontakt@gabrielakbarov.de>
2026-05-18 21:31:49 +0200

    fix sonar issues

--- frontend/src/app/components/global/navbar/navbar.component.ts
+++ frontend/src/app/components/global/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { BadgeDirective } from 'primeng/badge';
 import { Cart } from '@app/api/cart/cart.store';
