253988af50bf16ed6f6cc3a2c24b31313460b58c
Connor Schweighöfer <squidxtv@gmail.com>
2026-05-27 18:23:50 +0200

    Fix formatting in migration script

--- backend/src/main/resources/db/migration/V5__orders.sql
+++ backend/src/main/resources/db/migration/V5__orders.sql
@@ -1,25 +1,25 @@
 CREATE TABLE orders
 (
-    id                    UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
-    created_at            TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    customer_id           UUID REFERENCES accounts (id) ON DELETE CASCADE,
+    id                     UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    created_at             TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    customer_id            UUID REFERENCES accounts (id) ON DELETE CASCADE,
 
-    shipping_first_name   TEXT        NOT NULL,
-    shipping_last_name    TEXT        NOT NULL,
-    shipping_city         TEXT        NOT NULL,
-    shipping_postal_code  TEXT        NOT NULL,
-    shipping_street       TEXT        NOT NULL,
-    shipping_house_number TEXT        NOT NULL,
+    shipping_first_name    TEXT        NOT NULL,
+    shipping_last_name     TEXT        NOT NULL,
+    shipping_city          TEXT        NOT NULL,
+    shipping_postal_code   TEXT        NOT NULL,
+    shipping_street        TEXT        NOT NULL,
+    shipping_house_number  TEXT        NOT NULL,
 
-    payment_account_holder TEXT NOT NULL,
-    payment_iban TEXT NOT NULL,
+    payment_account_holder TEXT        NOT NULL,
+    payment_iban           TEXT        NOT NULL,
 
-    billing_first_name    TEXT,
-    billing_last_name     TEXT,
-    billing_city          TEXT,
-    billing_postal_code   TEXT,
-    billing_street        TEXT,
-    billing_house_number  TEXT,
+    billing_first_name     TEXT,
+    billing_last_name      TEXT,
+    billing_city           TEXT,
+    billing_postal_code    TEXT,
+    billing_street         TEXT,
+    billing_house_number   TEXT,
 
     CONSTRAINT billing_address_all_or_none CHECK (
         (billing_first_name IS NULL AND
@@ -53,7 +53,7 @@ CREATE TABLE order_items
 CREATE TABLE order_item_images
 (
     id               UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    order_item_id       UUID NOT NULL REFERENCES order_items (id) ON DELETE CASCADE,
+    order_item_id    UUID NOT NULL REFERENCES order_items (id) ON DELETE CASCADE,
     url              TEXT NOT NULL,
     alternative_text TEXT NOT NULL    DEFAULT ''
 );
