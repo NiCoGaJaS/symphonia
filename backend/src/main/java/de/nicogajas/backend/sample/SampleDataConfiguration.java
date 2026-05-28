9c6484d370c1c0f3e586609c2df43dc48363f59a
Yousername01 <jannik_michael.abram@edu.fhdw.de>
2026-05-28 22:41:11 +0200

    Implement suggestion

--- backend/src/main/java/de/nicogajas/backend/sample/SampleDataConfiguration.java
+++ backend/src/main/java/de/nicogajas/backend/sample/SampleDataConfiguration.java
@@ -36,7 +36,7 @@ public class SampleDataConfiguration {
             }
             logger.info("No products found. Inserting sample products.");
             try (InputStream stream = new ClassPathResource("db/sample-products.json").getInputStream()) {
-                List<Product> sampleProducts = json.readValue(stream, new TypeReference<List<Product>>() {});
+                List<Product> sampleProducts = json.readValue(stream, new TypeReference<>() {});
                 List<Product> saved = products.saveAll(sampleProducts);
                 logger.info("Successfully inserted {} sample products.", saved.size());
             } catch (Exception e) {
