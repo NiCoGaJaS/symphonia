67bf2c919711a50f61dc2beefdf68f1638c15671
Yousername01 <jannik_michael.abram@edu.fhdw.de>
2026-05-28 18:19:47 +0200

    Allow json comments, improve error handling

--- backend/src/main/java/de/nicogajas/backend/sample/SampleDataConfiguration.java
+++ backend/src/main/java/de/nicogajas/backend/sample/SampleDataConfiguration.java
@@ -9,6 +9,7 @@ import de.nicogajas.backend.security.authentication.Role;
 import java.io.InputStream;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
@@ -35,13 +36,15 @@ public class SampleDataConfiguration {
                 return;
             }
             logger.info("No products found. Inserting sample products.");
-            List<Product> sampleProducts;
             try (InputStream stream = new ClassPathResource("db/sample-products.json").getInputStream()) {
-                ObjectMapper json = new ObjectMapper();
+                List<Product> sampleProducts;
+                ObjectMapper json = new ObjectMapper().configure(JsonParser.Feature.ALLOW_COMMENTS, true);
                 sampleProducts = json.readValue(stream, new TypeReference<List<Product>>() {});
+                List<Product> saved = products.saveAll(sampleProducts);
+                logger.info("Successfully inserted {} sample products.", saved.size());
+            } catch (Exception e) {
+                logger.info("Could not insert sample products" + e);
             }
-            List<Product> saved = products.saveAll(sampleProducts);
-            logger.info("Successfully inserted {} sample products.", saved.size());
         };
     }
     
